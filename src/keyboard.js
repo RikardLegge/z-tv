7b3ea4443b375a0b99e01268b6f6d5ae8be77f1e
Rikard Legge <rikard.legge@gmail.com>
2019-09-03 23:37:34 +0200

    Add debounce

--- src/keyboard.js
+++ src/keyboard.js
@@ -2,12 +2,19 @@ const { useEffect } = require('react');
 
 function keymap(key) {
   const map = {
-    "1": ["q", "w", "e", "a", "s", "d", "z", "x"],
-    "2": ["r", "t", "y", "f", "g", "h", "c", "v", "b"],
-    "3": ["u", "i", "o", "j", "k", "l", "n", "m", ","],
+    "1": ["q", "w", "e", "a", "s", "d", "z", "x", "1"],
+    "2": ["r", "t", "y", "f", "g", "h", "c", "v", "b", "2"],
+    "3": ["u", "i", "o", "j", "k", "l", "n", "m", ",", "3"],
+    "4": ["4"],
+    "5": ["5"],
+    "6": ["6"],
+    "7": ["7"],
+    "8": ["8"],
+    "9": ["9"],
+    "0": ["0"],
+    "+": ["+"],
     "Enter": ["enter"],
     "Backspace": ["p", "å", "¨", "ö", "ä", "'", ".", "-"],
-    "+": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
   };
   key = key.toLowerCase();
 
@@ -20,12 +27,18 @@ function keymap(key) {
   }
 }
 
+let debounce = false;
 function useKeyboard(fn) {
   useEffect(() => {
     function keyDown(event) {
       event.preventDefault();
       const key = keymap(event.key);
       if(!key) return;
+
+      if(debounce) return;
+      debounce = true;
+      setTimeout(()=>debounce = false, 250);
+
       fn(key);
     }
     document.body.addEventListener("keydown", keyDown);
