62dcc1b6208cb63fafb0b466759a150d3e24b056
Rikard Legge <rikard.legge@gmail.com>
2019-09-03 14:46:45 +0200

    Update slideshow to auto update

--- src/views/SlidesView.js
+++ src/views/SlidesView.js
@@ -1,16 +1,30 @@
 const { html } = require('htm/react');
+const { useState, useEffect } = require('react');
 
-const url = "https://docs.google.com/presentation/d/e/2PACX-1vSqzhgcEbLVwCapnGT7FWeHeDvagdWR5ZX_qVt8_6wBjsC_YEiH5mE6Pi4OJxDCNX0-yAjqwCiStwyH/pub?start=true&loop=true&delayms=10000";
+const url = "https://docs.google.com/presentation/d/e/2PACX-1vSqzhgcEbLVwCapnGT7FWeHeDvagdWR5ZX_qVt8_6wBjsC_YEiH5mE6Pi4OJxDCNX0-yAjqwCiStwyH/pub?start=true&loop=true&delayms=10000&rm=minimal";
 
+const style = {
+  width: "100%",
+  height: "100%",
+  position: "absolute",
+  top: "0",
+  left: "0",
+};
 module.exports = function SlidesView(){
-  const style = {
-    width: "100%",
-    height: "100%",
-    position: "absolute",
-    top: "0",
-    left: "0",
-  };
-  return html`
-      <iframe src=${url} frameBorder="0" style=${style}></iframe>
-    `;
+  const newFrame = () => html`<iframe key=${Math.random()} src=${url} frameBorder="0" style=${style}></iframe>`;
+  const [frames, setFrames] = useState([newFrame(), newFrame()]);
+
+  useEffect(()=>{
+    function reverseFrames() {
+      console.log("Updated slideshow");
+      setFrames([
+        newFrame(),
+        frames[0]
+      ]);
+    }
+    const key = setTimeout(reverseFrames, 60000);
+    return ()=> clearTimeout(key);
+  }, [frames]);
+
+  return frames;
 };
