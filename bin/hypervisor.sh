aca243c29b435114bfe13d530437f1c89f6bff08
Rikard Legge <rikard.legge@gmail.com>
2019-09-03 14:24:45 +0200

    Improve hypervisor

--- bin/hypervisor.sh
+++ bin/hypervisor.sh
@@ -6,12 +6,18 @@ cd "$(dirname "$0")"
 while :
 do
 	sleep 10
-	git pull
-	if [[ "$?" -ne 0 ]]; then
+  if git checkout master &&
+     git fetch origin master &&
+     [ `git rev-list HEAD...origin/master --count` != 0 ]
+  then
+    git reset --hard origin/master
+    echo 'A new update is available!'
     echo "Killing electron process"
     ps -ef | grep electron | grep -v grep | awk '{print $2;}' | xargs kill -9
     echo "Starting new process"
     ./run.sh &
+  else
+    echo 'Nothing new'
   fi
 done
 
