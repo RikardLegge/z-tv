d3bf0bf6009a92767da24cbfe585c4d69b2d7a6c
Rikard Legge <rikard.legge@gmail.com>
2019-09-30 17:08:27 +0200

    Order export by timestamp

--- cmd/exportReport.js
+++ cmd/exportReport.js
@@ -2,7 +2,7 @@ const {connectToFirebase, getDB} = require("../src/bank");
 
 async function main() {
   await connectToFirebase({quiet: true});
-  const logs = await getDB().collection(`logs`).get();
+  const logs = await getDB().collection(`logs`).orderBy("timeStamp").get();
   console.log("TimeStamp\tAccount\tAmount");
   logs.forEach(log => {
     const {amount, timeStamp, account} = log.data();
